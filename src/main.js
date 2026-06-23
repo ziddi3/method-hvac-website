5b8b2fc0471d66350fae91d046d77020d3b95f3e
Ziddi C.J. <zidthesunkid@gmail.com>
2026-06-23 10:39:52 -0600

    Set contact form method

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './hardening.css'
 import { initializeSite } from './site.js'
 
 initializeSite(document.body.dataset.page ?? 'home')
+
+const contactForm = document.querySelector('[data-contact-form]')
+
+if (contactForm) {
+  contactForm.method = 'post'
+}
