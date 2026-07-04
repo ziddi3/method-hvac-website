1630f1493c2143dfa4c5fc0e8348708f75520012
Ziddi C.J. <zidthesunkid@gmail.com>
2026-07-04 03:49:56 -0600

    Load mobile polish on quote page

--- src/quote.js
+++ src/quote.js
@@ -1,6 +1,7 @@
 import './style.css'
 import './hardening.css'
 import './crm-workflow.css'
+import './mobile-quote-polish.css'
 import { initializeQuoteBuilder } from './quote-builder.js'
 import { initializeSite } from './site.js'
 
